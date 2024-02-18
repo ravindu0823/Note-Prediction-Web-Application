5e9ee1dc6a8b437ba7ab3c38f3affb60239059a2
ravindu0823 <guestpc87@gmail.com>
2024-02-18 12:23:07 +0530

    Created Backend and Developed the User routes

---
+++ server/models/user.js
@@ -0,0 +1,48 @@
+import pkg from "mongoose";
+const { Schema, model, models } = pkg;
+import bcrypt from "bcrypt";
+import mongooseUniqueValidator from "mongoose-unique-validator";
+
+//(Full Name, username, password, Contact Number, Address, NIC)
+
+const UserSchema = new Schema({
+  fullName: {
+    type: String,
+    required: [true, "Please enter Full Name"],
+  },
+
+  userName: {
+    type: String,
+    required: [true, "Please enter userName"],
+    unique: true,
+  },
+
+  email: {
+    type: String,
+    required: [true, "Please enter email"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Please enter password"],
+  },
+});
+
+//  Hash the password
+UserSchema.methods.generateHash = (password) => {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// Validate the password
+UserSchema.methods.validPassword = (password, dbPassword) => {
+  return bcrypt.compareSync(password, dbPassword);
+};
+
+// Validate the username
+UserSchema.plugin(mongooseUniqueValidator, {
+  message: "Username already exists. Duplicate key",
+});
+
+const User = models.Users || model("Users", UserSchema);
+
+export default User;
---
+++ server/routes/users.js
@@ -0,0 +1,145 @@
+import User from "../models/user.js";
+import { connectToDB } from "../db/conn.js";
+import express from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import {
+  validateUserAdd,
+  validateUserLogin,
+} from "../middlewares/userValidation.js";
+
+dotenv.config();
+const userRouter = express.Router();
+
+// Register new users
+userRouter.post("/register", validateUserAdd, async (req, res) => {
+  const { userData } = await req.body;
+
+  try {
+    await connectToDB();
+
+    const savedUser = new User(userData);
+
+    savedUser.password = savedUser.generateHash(userData.password);
+
+    await savedUser.save();
+
+    console.log(savedUser);
+    if (!savedUser) res.send("Not found").status(404);
+
+    const token = jwt.sign(
+      { userId: savedUser._id, fullName: savedUser.fullName },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "10m",
+      }
+    );
+
+    return res.status(201).json({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Login Existing Users
+userRouter.post("/login", validateUserLogin, async (req, res) => {
+  const { userData } = await req.body;
+  const { userName } = userData;
+  const { password } = userData;
+
+  try {
+    await connectToDB();
+
+    const loggedUser = await User.findOne({ userName });
+
+    if (!loggedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    if (!loggedUser.validPassword(password, loggedUser.password)) {
+      //password did not match
+      return res.status(401).json({ error: "Incorrect password" });
+    } else {
+      // password matched. proceed forward
+      console.log("password matched");
+      const token = jwt.sign(
+        { userId: loggedUser._id, fullName: loggedUser.fullName },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "10m",
+        }
+      );
+      return res.status(200).json({ token });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// Get all users
+userRouter.get("/", async (req, res) => {
+  try {
+    await connectToDB();
+
+    const users = await User.find();
+
+    if (users.length == 0) {
+      return res.status(400).json({ message: "Unauthorized" });
+    }
+
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// Authenticate user
+userRouter.get("/protected", async (req, res) => {
+  try {
+    const authHeader = await req.headers.authorization;
+    // console.log(authHeader);
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // console.log(decoded.userId);
+
+    if (!decoded) {
+      return res.status(400).json({ message: "Expired. Unauthorized" });
+    } else if (decoded.exp < Date.now() / 1000) {
+      return res.status(400).json({ message: "Expired. Unauthorized" });
+    } else {
+      // If the token is valid, return some protected data
+      return res.status(200).json({ data: "Protected data" });
+    }
+  } catch (error) {
+    console.log("Token Verification Error: ", error);
+    return res.status(400).json({ message: "Unauthorized" });
+  }
+});
+
+// Get user by id
+userRouter.get("/:userId", async (req, res) => {
+  const { userId } = req.params;
+
+  console.log(userId);
+
+  try {
+    await connectToDB();
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "No Users" });
+    }
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+export default userRouter;
