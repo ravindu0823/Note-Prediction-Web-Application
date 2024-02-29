0849cd0939be4d68c48d4e90a9d9b9b3c422899d
ravindu0823 <guestpc87@gmail.com>
2024-03-01 00:56:05 +0530

    Update User registration form and validation

--- client/src/models/User.js
+++ client/src/models/User.js
@@ -1,6 +1,8 @@
 export const UserData = {
-    fullName: String,
-    userName: String,
-    email: String,
-    password: String,
-}
+  fullName: null,
+  userName: null,
+  email: null,
+  password: null,
+  confirmPassword: null,
+  check: false,
+};
