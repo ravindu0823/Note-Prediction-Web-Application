b7ee2b8ae004b265f54f615e7792d60490bc5352
ravindu0823 <guestpc87@gmail.com>
2024-03-01 00:56:13 +0530

    Add user registration functionality

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,51 @@
 import { Button, Checkbox, Input, Typography } from "@material-tailwind/react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import musify_logo from "../assets/images/musify_logo.webp";
+import { useContext, useState } from "react";
+import { UserData } from "../models/User";
+import { SignInContext } from "../contexts/SignInContext";
+import { validateUserRegisterData } from "../utils/UserDataValidation";
+import axios, { USER_REGISTER } from "../api/axios";
+import Cookies from "js-cookie";
+import { ReactToast } from "../utils/ReactToast";
 
 const Register = () => {
+  const [userData, setUserData] = useState(UserData);
+  const navigate = useNavigate();
+  const { setLoggedIn } = useContext(SignInContext);
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    if (validateUserRegisterData(userData)) {
+      try {
+        const response = await axios.post(USER_REGISTER, { userData });
+
+        const { token } = response.data;
+        Cookies.set("token", token);
+
+        setLoggedIn(true);
+
+        ReactToast("Logged in successfully", "success");
+        navigate("/");
+      } catch (error) {
+        console.log(error);
+
+        if (error.response.status === 404) {
+          ReactToast("Invalid Username", "error");
+        }
+
+        if (error.response.status === 401) {
+          ReactToast("Invalid Password", "error");
+        }
+      }
+    }
+  };
+
   return (
     <>
       <div className="relative min-h-screen w-full">
-        <section className="bg-hero-image h-screen bg-cover flex pt-10">
+        <section className="bg-hero-image bg-opacity-5 h-screen bg-cover flex pt-10">
           <div className="w-full lg:w-3/5 mt-10">
             <div className="text-center">
               <Typography
@@ -25,7 +64,10 @@ const Register = () => {
                 Create your account to start using Musify.
               </Typography>
             </div>
-            <form className="mt-8 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2">
+            <form
+              className="mt-8 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2"
+              onSubmit={handleRegister}
+            >
               <div className="mb-1 flex flex-col gap-6">
                 <Typography
                   placeholder={"full name label"}
@@ -37,12 +79,15 @@ const Register = () => {
                 </Typography>
                 <Input
                   size="lg"
+                  value={userData.fullName}
+                  onChange={(e) =>
+                    setUserData({ ...userData, fullName: e.target.value })
+                  }
                   placeholder="John Doe"
                   className=" !border-t-blue-gray-200 focus:!border-light-blue-700 text-white placeholder-white"
                   labelProps={{
                     className: "before:content-none after:content-none",
                   }}
-                  crossOrigin={undefined}
                 />
 
                 <Typography
@@ -55,12 +100,15 @@ const Register = () => {
                 </Typography>
                 <Input
                   size="lg"
+                  value={userData.userName}
+                  onChange={(e) =>
+                    setUserData({ ...userData, userName: e.target.value })
+                  }
                   placeholder="johndoe"
                   className=" !border-t-blue-gray-200 focus:!border-light-blue-700 text-white placeholder-white"
                   labelProps={{
                     className: "before:content-none after:content-none",
                   }}
-                  crossOrigin={undefined}
                 />
 
                 <Typography
@@ -72,13 +120,16 @@ const Register = () => {
                   Your email
                 </Typography>
                 <Input
+                  value={userData.email}
+                  onChange={(e) =>
+                    setUserData({ ...userData, email: e.target.value })
+                  }
                   size="lg"
                   placeholder="someone@example.com"
                   className=" !border-t-blue-gray-200 focus:!border-light-blue-700 text-white placeholder-white"
                   labelProps={{
                     className: "before:content-none after:content-none",
                   }}
-                  crossOrigin={undefined}
                 />
 
                 <Typography
@@ -90,7 +141,10 @@ const Register = () => {
                   Password
                 </Typography>
                 <Input
-                  crossOrigin={undefined}
+                  value={userData.password}
+                  onChange={(e) =>
+                    setUserData({ ...userData, password: e.target.value })
+                  }
                   type="password"
                   size="lg"
                   placeholder="********"
@@ -109,7 +163,13 @@ const Register = () => {
                   Confirm Password
                 </Typography>
                 <Input
-                  crossOrigin={undefined}
+                  value={userData.confirmPassword}
+                  onChange={(e) =>
+                    setUserData({
+                      ...userData,
+                      confirmPassword: e.target.value,
+                    })
+                  }
                   type="password"
                   size="lg"
                   placeholder="********"
@@ -120,7 +180,10 @@ const Register = () => {
                 />
               </div>
               <Checkbox
-                crossOrigin={undefined}
+                value={userData.check}
+                onChange={(e) =>
+                  setUserData({ ...userData, check: e.target.checked })
+                }
                 className="checked:bg-light-blue-700 checked:border-white"
                 label={
                   <Typography
@@ -141,6 +204,7 @@ const Register = () => {
                 containerProps={{ className: "-ml-2.5" }}
               />
               <Button
+                type="submit"
                 placeholder={"button sign in"}
                 className="mt-6 bg-light-blue-700 hover:bg-light-blue-900"
                 fullWidth
