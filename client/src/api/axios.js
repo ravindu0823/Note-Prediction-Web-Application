5d6a71e76a1bdc15f1be43fc6ee602173e78e308
ravindu0823 <guestpc87@gmail.com>
2024-03-01 00:55:31 +0530

    Add USER_REGISTER endpoint to axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const USER_LOGIN = "users/login";
+export const USER_REGISTER = "users/register";
 
 export default axios.create({
   baseURL: import.meta.env.VITE_BASEURL,
