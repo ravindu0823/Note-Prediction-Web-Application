49f36a4d3d05fc8c6380719c2f78c6ea640cbeb9
ravindu0823 <guestpc87@gmail.com>
2024-03-15 09:06:08 +0530

    Add ArticleCard component

---
+++ client/src/components/ArticleCard.jsx
@@ -0,0 +1,34 @@
+import propTypes from "prop-types";
+
+const ArticleCard = ({ image, title, target }) => {
+  return (
+    <>
+      <article>
+        <a href={target}>
+          <img
+            src={image}
+            className="mb-5 w-full max-w-full rounded-lg"
+            alt="Image 1"
+          />
+        </a>
+        <h2 className="mb-2 text-xl font-bold leading-tight  text-white">
+          <a href={target}>{title}</a>
+        </h2>
+        <a
+          href="#"
+          className="inline-flex items-center font-medium underline underline-offset-4  text-primary-500 hover:no-underline"
+        >
+          Read more
+        </a>
+      </article>
+    </>
+  );
+};
+
+ArticleCard.propTypes = {
+  image: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+  target: propTypes.string.isRequired,
+};
+
+export default ArticleCard;
