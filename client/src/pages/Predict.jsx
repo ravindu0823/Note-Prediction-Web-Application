11603c1cef6844691e594fb9f16ff5b963ddd159
ravindu0823 <guestpc87@gmail.com>
2024-03-26 13:14:42 +0530

    Add region markers and update audio files in Predict component

--- client/src/pages/Predict.jsx
+++ client/src/pages/Predict.jsx
@@ -1,8 +1,14 @@
 import { useRef, useEffect, useMemo } from "react";
-import audioFile from "../assets/audio/audio.mp3";
 import { useWavesurfer } from "@wavesurfer/react";
 import RegionsPlugin from "wavesurfer.js/dist/plugins/regions.esm.js";
 import Timeline from "wavesurfer.js/dist/plugins/timeline.esm.js";
+import { ComplexNavbar } from "../components/NavBar";
+import { Button } from "@material-tailwind/react";
+import {
+  FinalCountDownLabData,
+  ThousandYearsLabData,
+} from "../utils/TestLabData";
+import { finalCountDownAudio, thousandYearsAudio } from "../assets/audio";
 
 const Predict = () => {
   const containerRef = useRef();
@@ -10,32 +16,33 @@ const Predict = () => {
   const randomColor = () =>
     `rgba(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)}, 0.5)`;
 
+  const createRegion = (start, end, content) => {
+    return {
+      start,
+      end,
+      content,
+      color: randomColor(),
+      drag: false,
+      resize: false,
+    };
+  };
+
   const addRegionMarker = () => {
     const regions = wavesurfer.registerPlugin(RegionsPlugin.create());
 
-    regions.addRegion({
-      start: 0,
-      end: 8,
-      content: "Resize me",
-      color: randomColor(),
-      drag: false,
-      resize: true,
-    });
-
-    regions.addRegion({
-      start: 10,
-      end: 20,
-      content: "Resize me",
-      color: randomColor(),
-      drag: false,
-      resize: true,
+    ThousandYearsLabData.chords.forEach((region) => {
+      regions.addRegion(createRegion(region.start, region.end, region.note));
     });
   };
 
   const { wavesurfer, isReady, isPlaying, currentTime } = useWavesurfer({
     container: containerRef,
-    url: audioFile,
+    url: thousandYearsAudio,
     waveColor: "green",
+    progressColor: "purple",
+    // autoScroll: true,
+    scrollParent: true,
+    minPxPerSec: 100,
     height: 100,
     plugins: useMemo(() => [Timeline.create(), RegionsPlugin.create()], []),
   });
@@ -53,17 +60,18 @@ const Predict = () => {
     }
   }, [isReady, wavesurfer]);
 
-  /* wavesurfer.on("decode", () => {
-    console.log("Audio decoded");
-  }) */
-
   return (
     <>
-      <div ref={containerRef} />
+      <div>
+        <ComplexNavbar className="mx-auto max-w-screen-xl p-7" />
+        <div ref={containerRef} className="mt-10" />
 
-      <button onClick={onPlayPause}>{isPlaying ? "Pause" : "Play"}</button>
+        <Button onClick={onPlayPause} color="blue" className="m-2">
+          {isPlaying ? "Pause" : "Play"}
+        </Button>
 
-      <button onClick={addRegionMarker}>Add Region</button>
+        <button onClick={addRegionMarker}>Add Region</button>
+      </div>
     </>
   );
 };
