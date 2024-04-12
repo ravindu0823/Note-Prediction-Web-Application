1a08952aadc31b8177024c0100a0fe8beab2bb5e
ravindu0823 <guestpc87@gmail.com>
2024-04-12 22:41:14 +0530

    Add new audio files and update audio index.js

--- client/src/assets/audio/index.js
+++ client/src/assets/audio/index.js
@@ -1,2 +1,4 @@
 export { default as finalCountDownAudio } from "./acc.mp3";
 export { default as thousandYearsAudio } from "./audio.mp3";
+export { default as thousandYearsAudioForNotes } from "./A-Thousand-Years-Christina-Perri.mp3";
+export { default as CantHelpFallingInLove } from "./piano.wav";
