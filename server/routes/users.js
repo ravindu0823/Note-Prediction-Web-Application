c54ef0b67fa6c517150da24b46866ecec41ecf6f
ravindu0823 <guestpc87@gmail.com>
2024-04-15 01:51:10 +0530

    Refactor user routes to include update, reactivate, and delete endpoints

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 import {
   validateUserAdd,
   validateUserLogin,
+  validateUserUpdate,
 } from "../middlewares/userValidation.js";
 
 dotenv.config();
@@ -72,6 +73,70 @@ userRouter.post("/login", validateUserLogin, async (req, res) => {
       );
       return res.status(200).json({ token });
     }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+userRouter.put("/update/:userId", validateUserUpdate, async (req, res) => {
+  const { userId } = req.params;
+  const { userData } = await req.body;
+
+  try {
+    await connectToDB();
+
+    const updatedUser = await User.findByIdAndUpdate(userId, userData);
+
+    if (!updatedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const newUser = await User.findById(userId);
+
+    return res.status(200).json({ newUser });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+userRouter.put("/reactivate/:userId", async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    await connectToDB();
+
+    const reactivatedUser = await User.findByIdAndUpdate(userId, {
+      status: "Active",
+    });
+
+    if (!reactivatedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    return res.status(200).json({ message: "User reactivated" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+userRouter.delete("/delete/:userId", async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    await connectToDB();
+
+    const deletedUser = await User.findByIdAndUpdate(userId, {
+      status: "Deleted",
+    });
+
+    if (!deletedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    return res.status(200).json({ message: "User deleted" });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: error.message });
