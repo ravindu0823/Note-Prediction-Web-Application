2c2665fea688062b01d6a1e4edd6ee9b5ab041b7
ravindu0823 <guestpc87@gmail.com>
2024-04-15 01:49:16 +0530

    Refactor user page to fetch and display user data dynamically

--- admin/app/(dashboard)/dashboard/user/[userId]/page.tsx
+++ admin/app/(dashboard)/dashboard/user/[userId]/page.tsx
@@ -1,25 +1,50 @@
+"use client";
+import axios, { GET_ALL_USERS } from "@/axios/axios";
 import BreadCrumb from "@/components/breadcrumb";
 import { ProductForm } from "@/components/forms/product-form";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import React from "react";
+import { User } from "@/constants/data";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const breadcrumbItems = [
+  { title: "User", link: "/dashboard/user" },
+  { title: "Create", link: "/dashboard/user/create" },
+];
 
 export default function Page() {
-  const breadcrumbItems = [
-    { title: "User", link: "/dashboard/user" },
-    { title: "Create", link: "/dashboard/user/create" },
-  ];
+  const [userData, setUserData] = useState<User>();
+  const { userId } = useParams();
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(`${GET_ALL_USERS}/${userId}`);
+      const { user } = await response.data;
+      setUserData(user);
+    };
+
+    getData();
+  }, [userId]);
+
   return (
     <ScrollArea className="h-full">
       <div className="flex-1 space-y-4 p-5">
         <BreadCrumb items={breadcrumbItems} />
-        <ProductForm
-          categories={[
-            { _id: "shirts", name: "shirts" },
-            { _id: "pants", name: "pants" },
-          ]}
-          initialData={null}
-          key={null}
-        />
+        {userData && (
+          <ProductForm
+            categories={[
+              { _id: "shirts", name: "shirts" },
+              { _id: "pants", name: "pants" },
+            ]}
+            initialData={{
+              _id: userData._id,
+              username: userData.userName,
+              fullName: userData.fullName,
+              email: userData.email,
+            }}
+            key={null}
+          />
+        )}
       </div>
     </ScrollArea>
   );
