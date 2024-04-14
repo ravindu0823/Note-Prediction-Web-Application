371e741600e52f7bd2fd495b0a8e13ab51e06574
ravindu0823 <guestpc87@gmail.com>
2024-04-15 01:49:54 +0530

    Add new API endpoints for user management

--- admin/axios/axios.ts
+++ admin/axios/axios.ts
@@ -3,6 +3,10 @@ import axios from "axios";
 export const ADMIN_LOGIN = "admin/login";
 export const GET_ALL_USERS = "users";
 export const GET_ALL_NEWS = "news";
+export const UPDATE_USERS = "users/update";
+export const CREATE_USER = "users/register";
+export const DELETE_USER = "users/delete";
+export const REACTIVATE_USER = "users/reactivate";
 
 export default axios.create({
   baseURL: process.env.NEXT_PUBLIC_SERVER_BASEURL,
