d1bc426ec2f18e7296f9ac016b89168beddd055e
ravindu0823 <guestpc87@gmail.com>
2024-04-15 01:50:21 +0530

    Refactor user table cell action component to handle user deletion and reactivation

--- admin/components/tables/user-tables/cell-action.tsx
+++ admin/components/tables/user-tables/cell-action.tsx
@@ -1,4 +1,5 @@
 "use client";
+import axios, { DELETE_USER, REACTIVATE_USER } from "@/axios/axios";
 import { AlertModal } from "@/components/modal/alert-modal";
 import { Button } from "@/components/ui/button";
 import {
@@ -8,6 +9,7 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { toast } from "@/components/ui/use-toast";
 import { User } from "@/constants/data";
 import { Edit, MoreHorizontal, Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
@@ -19,17 +21,57 @@ interface CellActionProps {
 
 export const CellAction: React.FC<CellActionProps> = ({ data }) => {
   const [loading, setLoading] = useState(false);
-  const [open, setOpen] = useState(false);
+  const [deleteOpen, setDeleteOpen] = useState(false);
+  const [reactivateOpen, setReactivateOpen] = useState(false);
   const router = useRouter();
 
-  const onConfirm = async () => {};
+  const onDeleteConfirm = async () => {
+    setLoading(true);
+
+    const response = await axios.delete(`${DELETE_USER}/${data._id}`);
+    if (response.status === 200) {
+      toast({
+        variant: "default",
+        title: "Success",
+        description: "User deleted.",
+      });
+    }
+
+    setLoading(false);
+    setDeleteOpen(false);
+    router.push("/dashboard");
+  };
+
+  const onReactivateConfirm = async () => {
+    setLoading(true);
+
+    const response = await axios.put(`${REACTIVATE_USER}/${data._id}`);
+    if (response.status === 200) {
+      toast({
+        variant: "default",
+        title: "Success",
+        description: "User reactivated.",
+      });
+    }
+
+    setLoading(false);
+    setReactivateOpen(false);
+    router.push("/dashboard");
+  };
 
   return (
     <>
       <AlertModal
-        isOpen={open}
-        onClose={() => setOpen(false)}
-        onConfirm={onConfirm}
+        isOpen={deleteOpen}
+        onClose={() => setDeleteOpen(false)}
+        onConfirm={onDeleteConfirm}
+        loading={loading}
+      />
+
+      <AlertModal
+        isOpen={reactivateOpen}
+        onClose={() => setReactivateOpen(false)}
+        onConfirm={onReactivateConfirm}
         loading={loading}
       />
       <DropdownMenu modal={false}>
@@ -42,14 +84,24 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
         <DropdownMenuContent align="end">
           <DropdownMenuLabel>Actions</DropdownMenuLabel>
 
-          <DropdownMenuItem
-            onClick={() => router.push(`/dashboard/user/${data._id}`)}
-          >
-            <Edit className="mr-2 h-4 w-4" /> Update
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setOpen(true)}>
-            <Trash className="mr-2 h-4 w-4" /> Delete
-          </DropdownMenuItem>
+          {data.status === "Active" ? (
+            <>
+              <DropdownMenuItem
+                onClick={() => router.push(`/dashboard/user/${data._id}`)}
+              >
+                <Edit className="mr-2 h-4 w-4" /> Update
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setDeleteOpen(true)}>
+                <Trash className="mr-2 h-4 w-4" /> Delete
+              </DropdownMenuItem>
+            </>
+          ) : (
+            <>
+              <DropdownMenuItem onClick={() => setReactivateOpen(true)}>
+                <Trash className="mr-2 h-4 w-4" /> Reactivate
+              </DropdownMenuItem>
+            </>
+          )}
         </DropdownMenuContent>
       </DropdownMenu>
     </>
