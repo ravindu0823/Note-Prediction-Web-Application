d210305ad3629a88d7d81a0339dfac3cf0463aac
ravindu0823 <guestpc87@gmail.com>
2024-04-15 01:51:01 +0530

    Refactor userValidation middleware to include user update validation

--- server/middlewares/userValidation.js
+++ server/middlewares/userValidation.js
@@ -1,7 +1,12 @@
 export const validateUserAdd = (req, res, next) => {
   const { userData } = req.body;
 
-  if (!userData.userName && !userData.password && !userData.fullName && userData.email) {
+  if (
+    !userData.userName &&
+    !userData.password &&
+    !userData.fullName &&
+    !userData.email
+  ) {
     return res.status(400).json({ error: "Please enter all fields" });
   }
 
@@ -37,6 +42,29 @@ export const validateUserLogin = (req, res, next) => {
 
   if (!userData.password) {
     return res.status(400).json({ error: "Please enter your password" });
+  }
+
+  next();
+};
+
+export const validateUserUpdate = (req, res, next) => {
+  const { userId } = req.params;
+  const { userData } = req.body;
+
+  if (!userData.fullName && userData.email) {
+    return res.status(400).json({ error: "Please enter all fields" });
+  }
+
+  if (!userId) {
+    return res.status(400).json({ error: "Please enter your userId" });
+  }
+
+  if (!userData.fullName) {
+    return res.status(400).json({ error: "Please enter your fullName" });
+  }
+
+  if (!userData.email) {
+    return res.status(400).json({ error: "Please enter your email" });
   }
 
   next();
