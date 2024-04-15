22a6624b9401fbeb9083a102bcf86b61631a3e2e
ravindu0823 <guestpc87@gmail.com>
2024-04-16 00:54:58 +0530

    Refactor file-upload.tsx to fix src attribute assignment

--- admin/components/file-upload.tsx
+++ admin/components/file-upload.tsx
@@ -52,7 +52,7 @@ export default function FileUpload({
                   fill
                   className="object-cover"
                   alt="Image"
-                  src={item.fileUrl || ""}
+                  src={item as any || ""}
                 />
               </div>
             </div>
