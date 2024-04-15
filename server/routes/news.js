53ed643894a6bd52b5529642abb7242181391548
ravindu0823 <guestpc87@gmail.com>
2024-04-16 00:56:45 +0530

    Add CRUD functionality to news routes

--- server/routes/news.js
+++ server/routes/news.js
@@ -3,11 +3,15 @@ import { connectToDB } from "../db/conn.js";
 import express from "express";
 import dotenv from "dotenv";
 import axios from "axios";
+import {
+  newsAddValidation,
+  newsUpdateValidation,
+} from "../middlewares/newsValidation.js";
 
 dotenv.config();
 const newsRouter = express.Router();
 
-// Register new users
+// Add all news from scrape
 newsRouter.post("/addAll", async (req, res) => {
   try {
     await connectToDB();
@@ -31,6 +35,85 @@ newsRouter.post("/addAll", async (req, res) => {
   }
 });
 
+// Add news manually
+newsRouter.post("/add", newsAddValidation, async (req, res) => {
+  const { category, title, target, desc, date, image } = req.body;
+
+  try {
+    await connectToDB();
+
+    const news = new News({
+      category,
+      title,
+      target,
+      desc,
+      date,
+      image,
+    });
+
+    await news.save();
+
+    if (!news) {
+      return res.status(404).json({ error: "News not found" });
+    }
+
+    return res.status(201).json(news);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Update news manually
+newsRouter.put("/update/:newsId", newsUpdateValidation, async (req, res) => {
+  const { newsId } = req.params;
+  const { category, title, target, desc, date, image } = req.body;
+
+  try {
+    await connectToDB();
+
+    const news = await News.findByIdAndUpdate(newsId, {
+      category,
+      title,
+      target,
+      desc,
+      date,
+      image,
+    });
+
+    if (!news) {
+      return res.status(404).json({ error: "News not found" });
+    }
+
+    const updatedNews = await News.findById(newsId);
+
+    return res.status(200).json(updatedNews);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+newsRouter.delete("/delete/:newsId", async (req, res) => {
+  const { newsId } = req.params;
+
+  try {
+    await connectToDB();
+
+    const news = await News.findByIdAndDelete(newsId);
+
+    if (!news) {
+      return res.status(404).json({ error: "News not found" });
+    }
+
+    return res.status(200).json({ message: "News deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Get all news
 newsRouter.get("/", async (req, res) => {
   try {
     await connectToDB();
@@ -39,6 +122,26 @@ newsRouter.get("/", async (req, res) => {
     const news = await News.find();
 
     return res.status(200).json({ news });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Get news by id
+newsRouter.get("/:newsId", async (req, res) => {
+  try {
+    await connectToDB();
+
+    const { newsId } = req.params;
+
+    const news = await News.findById(newsId);
+
+    if (!news) {
+      return res.status(404).json({ error: "News not found" });
+    }
+
+    return res.status(200).json(news);
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
