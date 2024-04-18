29feb717ea7f4092d0cb641eff976d839e8408be
ravindu0823 <guestpc87@gmail.com>
2024-04-19 01:03:23 +0530

    Add SignInContextData.js to implement sign-in functionality in the client application

---
+++ client/src/contexts/SignInContextData.js
@@ -0,0 +1,11 @@
+export const AuthContextData = {
+    user: null,
+    setUser: () => {},
+    isSignedIn: false,
+    setIsSignedIn: () => {},
+    signIn: () => {},
+    signOut: () => {},
+    signUp: () => {},
+    error: null,
+    setError: () => {},
+}
