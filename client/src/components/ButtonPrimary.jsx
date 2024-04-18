bd85c065d5a01f553140d7d62d40919aff787483
ravindu0823 <guestpc87@gmail.com>
2024-04-19 01:02:23 +0530

    Refactor ButtonPrimary component to use Material-UI Button in ButtonPrimary.jsx

--- client/src/components/ButtonPrimary.jsx
+++ client/src/components/ButtonPrimary.jsx
@@ -1,15 +1,19 @@
+import { Button } from "@material-tailwind/react";
 import propTypes from "prop-types";
 
-const ButtonPrimary = ({ children, addClass }) => {
+const ButtonPrimary = ({ children, addClass, onClick }) => {
   return (
-    <button
+    <Button
+      size="lg"
+      onClick={onClick}
+      color="light-blue"
       className={
         "py-3 lg:py-4 px-12 lg:px-16 text-white-500 font-semibold rounded-lg bg-light-blue-700 hover:shadow-orange-md transition-all outline-none " +
         addClass
       }
     >
       {children}
-    </button>
+    </Button>
   );
 };
 
