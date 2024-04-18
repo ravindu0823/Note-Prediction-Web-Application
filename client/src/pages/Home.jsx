25fee9fe20284ba0e24c13a0a16fe844f2f1d377
ravindu0823 <guestpc87@gmail.com>
2024-04-19 01:05:49 +0530

    Refactor background images in Home and Hero components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,21 +1,12 @@
-import { useContext } from "react";
 import { ComplexNavbar } from "../components/NavBar";
-import { SignInContext } from "../contexts/SignInContext";
-import Cookies from "js-cookie";
 import Hero from "../components/Hero";
 import FeaturesComponent from "../components/Features";
 
 const Home = () => {
-  const { setLoggedIn } = useContext(SignInContext);
-
-  if (Cookies.get("token")) {
-    setLoggedIn(true);
-  }
-
   return (
     <>
       <div className="bg-[#111827] text-white">
-        <div className="bg-hero-image h-screen bg-no-repeat bg-cover bg-center bg-gray-400 bg-blend-multiply">
+        <div className="bg-hero-image h-screen bg-no-repeat bg-cover bg-center bg-gray-700 bg-blend-multiply">
           <div className="mx-auto max-w-screen-xl">
             <ComplexNavbar className="p-7" />
             <Hero />
