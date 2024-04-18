99264539bbefef93f14ed121ff353fa2347d9c4a
ravindu0823 <guestpc87@gmail.com>
2024-04-19 01:03:07 +0530

    Refactor ProfileMenu component to add dynamic profile menu items in ProfileMenu.jsx

---
+++ client/src/components/ProfileMenu.jsx
@@ -0,0 +1,82 @@
+import { ChevronDownIcon } from "@heroicons/react/24/solid";
+import {
+  Avatar,
+  Button,
+  Menu,
+  MenuHandler,
+  MenuItem,
+  MenuList,
+  Typography,
+} from "@material-tailwind/react";
+import propTypes from "prop-types";
+import { createElement, useState } from "react";
+
+const ProfileMenu = ({ profileMenuItems }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
+      <MenuHandler>
+        <Button
+          variant="text"
+          placeholder={true}
+          color="blue-gray"
+          className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto"
+        >
+          <Avatar
+            variant="circular"
+            size="sm"
+            placeholder={true}
+            alt="tania andrew"
+            className="border border-gray-900 p-0.5"
+            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+          />
+          <ChevronDownIcon
+            strokeWidth={2.5}
+            className={`h-3 w-3 transition-transform ${
+              isMenuOpen ? "rotate-180" : ""
+            }`}
+          />
+        </Button>
+      </MenuHandler>
+
+      <MenuList className="p-1" placeholder={true}>
+        {profileMenuItems.map(({ label, icon, onclick }, key) => {
+          const isLastItem = key === profileMenuItems.length - 1;
+          return (
+            <MenuItem
+              placeholder={"true"}
+              key={label}
+              onClick={onclick}
+              className={`flex items-center gap-2 rounded ${
+                isLastItem
+                  ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
+                  : ""
+              }`}
+            >
+              {createElement(icon, {
+                className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
+                strokeWidth: 2,
+              })}
+              <Typography
+                placeholder={"true"}
+                as="span"
+                variant="small"
+                className="font-normal"
+                color={isLastItem ? "red" : "inherit"}
+              >
+                {label}
+              </Typography>
+            </MenuItem>
+          );
+        })}
+      </MenuList>
+    </Menu>
+  );
+};
+
+ProfileMenu.propTypes = {
+  profileMenuItems: propTypes.array.isRequired,
+};
+
+export default ProfileMenu;
