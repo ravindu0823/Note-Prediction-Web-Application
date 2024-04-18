9b0375df3b2ec784e0e679117645596d6974312b
ravindu0823 <guestpc87@gmail.com>
2024-04-19 01:00:13 +0530

    Add new prediction analysis endpoints to axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const USER_LOGIN = "users/login";
 export const USER_REGISTER = "users/register";
+export const USER_VALIDATE = "users/protected";
 export const GET_ARTICLES = "news";
 export const ANALYZE_CHORDS = "predict/analyzeChords";
 export const ANALYZE_NOTES = "predict/analyzeNotes";
