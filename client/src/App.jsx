2bda69b76d1c326e6eb2fc47f38973ea4781704c
ravindu0823 <guestpc87@gmail.com>
2024-04-20 04:33:29 +0530

    Refactor routing in App.jsx to use PrivateRoute and AuthContextProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,39 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import { Toaster } from "react-hot-toast";
-import { useState } from "react";
-import { SignInContext } from "./contexts/SignInContext";
 import Articles from "./pages/Articles";
 import Footer from "./components/Footer";
 import Predict from "./pages/Predict";
 import Feedback from "./pages/Feedback";
 import AboutUs from "./pages/AboutUs";
+import { AuthContextProvider } from "./contexts/AuthContext";
+import { PrivateRoute, PrivateRouteForAuth } from "./contexts/PrivateRoute";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-
   return (
     <>
       <BrowserRouter>
-        <SignInContext.Provider value={{ loggedIn, setLoggedIn }}>
+        <AuthContextProvider>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route index element={<Home />} />
             <Route path="/articles" element={<Articles />} />
-            <Route path="/predict" element={<Predict />} />
             <Route path="/about" element={<AboutUs />} />
             <Route path="/feedback" element={<Feedback />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/predict" element={<Predict />} />
+            </Route>
+            <Route element={<PrivateRouteForAuth />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
           <Footer />
-        </SignInContext.Provider>
-        <Toaster position="bottom-right" reverseOrder={false} />
+          <Toaster position="bottom-right" reverseOrder={false} />
+        </AuthContextProvider>
       </BrowserRouter>
     </>
   );
