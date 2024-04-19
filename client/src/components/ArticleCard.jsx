596218591dee96059051acde87da492ba3f1bd95
ravindu0823 <guestpc87@gmail.com>
2024-04-20 04:34:13 +0530

    Refactor ArticleCard component to include category, description, and date in client/src/components/ArticleCard.jsx

--- client/src/components/ArticleCard.jsx
+++ client/src/components/ArticleCard.jsx
@@ -1,29 +1,63 @@
+import { Typography } from "@material-tailwind/react";
 import propTypes from "prop-types";
+import { easeIn, motion } from "framer-motion";
+import { useMemo } from "react";
+import getScrollAnimation from "../utils/getScrollAnimation";
+import ScrollAnimationWrapper from "./ScrollAnimationWrapper";
 
-const ArticleCard = ({ image, title, target }) => {
+const ArticleCard = ({ image, title, target, category, desc, date }) => {
+  const scrollAnimation = useMemo(() => getScrollAnimation(), []);
   return (
-    <>
-      <article>
+    <ScrollAnimationWrapper>
+      <motion.div
+        variants={scrollAnimation}
+        className="p-7  rounded-lg border shadow-md bg-[#111827] border-cyan-500"
+        custom={{
+          duration: 2,
+          delay: 1,
+        }}
+      >
         <a href={target} target="_blank">
           <img
+            className="mb-5 rounded-lg w-full text-white"
             src={image}
-            className="mb-5 w-full max-w-full rounded-lg"
-            alt="Image 1"
+            alt={`Image of ${title}`}
           />
         </a>
-        <h2 className="mb-2 text-xl font-bold leading-tight  text-white">
-          <a href={target} target="_blank">
-            {title}
-          </a>
-        </h2>
-        <a
-          href="#"
-          className="inline-flex items-center font-medium underline underline-offset-4  text-primary-500 hover:no-underline"
+        <span className="text-xs font-semibold mr-2 px-2.5 py-0.5 rounded bg-blue-800 text-white">
+          {category}
+        </span>
+        <Typography
+          as={"a"}
+          href={target}
+          target="_blank"
+          variant="h4"
+          className="my-2 font-bold tracking-tight text-white"
         >
-          Read more
-        </a>
-      </article>
-    </>
+          {title}
+        </Typography>
+        <Typography
+          variant="paragraph"
+          className="mb-4 font-light text-gray-400"
+        >
+          {desc && desc.substring(0, 100)}{" "}
+          <a href={target} className="text-blue-300 underline">
+            See more...
+          </a>
+        </Typography>
+        <div className="flex items-center space-x-4">
+          <img
+            className="w-10 h-10 rounded-full"
+            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png"
+            alt="Sofia McGuire avatar"
+          />
+          <div className="font-medium text-white">
+            <div>Medical Express</div>
+            <div className="text-sm font-normal text-gray-400">{date}</div>
+          </div>
+        </div>
+      </motion.div>
+    </ScrollAnimationWrapper>
   );
 };
 
@@ -31,6 +65,9 @@ ArticleCard.propTypes = {
   image: propTypes.string.isRequired,
   title: propTypes.string.isRequired,
   target: propTypes.string.isRequired,
+  category: propTypes.string.isRequired,
+  desc: propTypes.string.isRequired,
+  date: propTypes.string.isRequired,
 };
 
 export default ArticleCard;
