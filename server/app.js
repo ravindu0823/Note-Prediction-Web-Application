510501cd7aec6e10f5f63b80b9aa2e25365423a5
ravindu0823 <guestpc87@gmail.com>
2024-04-20 04:36:14 +0530

    Update CORS origin URLs in server/app.js

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5000"],
+  origin: ["http://localhost:8080", "http://localhost:5000"],
   credentials: true,
   optionSuccessStatus: 200,
 };
