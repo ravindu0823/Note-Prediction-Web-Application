53679d3f93064b996afd4d605f4f01099062a32e
ravindu0823 <guestpc87@gmail.com>
2024-04-20 04:35:37 +0530

    Refactor Articles page UI and update background image

--- client/src/pages/Articles.jsx
+++ client/src/pages/Articles.jsx
@@ -4,12 +4,10 @@ import { ComplexNavbar } from "../components/NavBar";
 const Articles = () => {
   return (
     <>
-      <div className="bg-[#111827] text-white">
-        <div className="bg-hero-image h-screen bg-cover">
-          <div className="mx-auto max-w-screen-xl">
-            <ComplexNavbar className="p-7" />
-            <Article />
-          </div>
+      <div className="bg-article-image min-h-screen bg-center bg-gray-500 bg-blend-multiply">
+        <div className="mx-auto max-w-screen-xl">
+          <ComplexNavbar className="p-7" />
+          <Article />
         </div>
       </div>
     </>
