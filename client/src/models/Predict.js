ab6dad5e33c946d51e85a608f6165d8f41c85d20
ravindu0823 <guestpc87@gmail.com>
2024-04-20 23:28:44 +0530

    Add PredictSchema model in Predict.js

---
+++ client/src/models/Predict.js
@@ -0,0 +1,22 @@
+export const PredictSchema = {
+  _id: null,
+  songId: {
+    _id: null,
+    songPath: null,
+    userId: null,
+  },
+  chords: [
+    {
+      end: null,
+      note: null,
+      start: null,
+    },
+  ],
+  notes: [
+    {
+      end: null,
+      note: null,
+      start: null,
+    },
+  ],
+};
