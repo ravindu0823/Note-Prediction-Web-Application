423e89352e304eabde69f42847170a5015789566
ravindu0823 <guestpc87@gmail.com>
2024-04-20 23:31:36 +0530

    Update FeedbackSchema model in Feedback.js

--- server/models/feedback.js
+++ server/models/feedback.js
@@ -2,15 +2,34 @@ import pkg from "mongoose";
 const { Schema, model, models } = pkg;
 
 const FeedbackSchema = new Schema({
-  userId: {
-    type: Types.ObjectId,
-    ref: "Users",
-    required: [true, "Please enter user"],
+  firstName: {
+    type: String,
+    required: [true, "Please enter firstName"],
+  },
+
+  lastName: {
+    type: String,
+    required: [true, "Please enter lastName"],
+  },
+
+  email: {
+    type: String,
+    required: [true, "Please enter email"],
+  },
+
+  phoneNumber: {
+    type: String,
+    required: [true, "Please enter phoneNumber"],
   },
 
   feedback: {
     type: String,
     required: [true, "Please enter feedback"],
+  },
+
+  status: {
+    type: String,
+    default: "Active",
   },
 });
 
