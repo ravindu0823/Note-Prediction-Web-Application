1224b45c40f7abc3adbf8ede5d6d88ac10a862be
ravindu0823 <guestpc87@gmail.com>
2024-04-20 23:28:24 +0530

    Add error handling and toasts in PrivateRoute.jsx

--- client/src/contexts/PrivateRoute.jsx
+++ client/src/contexts/PrivateRoute.jsx
@@ -1,15 +1,24 @@
 import { useContext } from "react";
 import { AuthContext } from "./AuthContext";
 import { Navigate, Outlet } from "react-router-dom";
+import { ReactToast } from "../utils/ReactToast";
 
 export const PrivateRoute = () => {
   const { isSignedIn } = useContext(AuthContext);
+  if (!isSignedIn) {
+    ReactToast("You must be signed in to view this page.", "error");
+    return <Navigate to="/login" />;
+  }
 
-  return isSignedIn ? <Outlet /> : <Navigate to="/login" />;
+  return <Outlet />;
 };
 
 export const PrivateRouteForAuth = () => {
   const { isSignedIn } = useContext(AuthContext);
+  if (isSignedIn) {
+    ReactToast("You are already signed in.", "error");
+    return <Navigate to="/" />;
+  }
 
-  return isSignedIn ? <Navigate to="/" /> : <Outlet />;
+  return <Outlet />;
 };
