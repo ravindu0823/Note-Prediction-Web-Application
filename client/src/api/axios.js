52e72c32c95d4e8cdf3d1454c8cd99e78a013637
ravindu0823 <guestpc87@gmail.com>
2024-04-20 23:25:01 +0530

    Add GET_HISTORY and CREATE_FEEDBACK API endpoints

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -4,9 +4,12 @@ export const USER_LOGIN = "users/login";
 export const USER_REGISTER = "users/register";
 export const USER_VALIDATE = "users/protected";
 export const GET_ARTICLES = "news";
+export const GET_HISTORY = "predict";
+export const GET_USER = "users";
 export const ANALYZE_CHORDS = "predict/analyzeChords";
 export const ANALYZE_NOTES = "predict/analyzeNotes";
 export const ANALYZE_BOTH = "predict/analyzeBoth";
+export const CREATE_FEEDBACK = "feedback/add";
 
 export default axios.create({
   baseURL: import.meta.env.VITE_BASEURL,
