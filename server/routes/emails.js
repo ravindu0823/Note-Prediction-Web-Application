bc700291e08fa247eee0b8c3211002f91aede424
ravindu0823 <guestpc87@gmail.com>
2024-04-25 03:58:40 +0530

    Add emailRouter to handle sending emails

---
+++ server/routes/emails.js
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+
+dotenv.config();
+const emailRouter = express.Router();
+
+const transporter = nodemailer.createTransport({
+  service: process.env.NODEMAILER_SERVICE_PROVIDER,
+  host: process.env.NODEMAILER_HOST,
+  port: process.env.NODEMAILER_PORT,
+  secure: true,
+  auth: {
+    user: process.env.NODEMAILER_USER,
+    pass: process.env.NODEMAILER_PASS,
+  },
+});
+
+emailRouter.post("/send", async (req, res) => {
+  const { emailHtml, userEmail } = req.body;
+
+  try {
+    const options = {
+      from: "Musify <musify@gmail.com>",
+      to: userEmail,
+      subject: "Musify Newsletter",
+      html: emailHtml,
+    };
+
+    const email = await transporter.sendMail(options);
+
+    if (!email) {
+      return res.status(404).json({ error: "Email not sent" });
+    }
+
+    return res.status(201).json({ email });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+export default emailRouter;
