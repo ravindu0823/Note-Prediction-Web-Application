f6ec5e502226f3885329385bf97f1802fc2b9633
ravindu0823 <guestpc87@gmail.com>
2024-05-07 02:56:33 +0530

    Refactor email functionality in emails.js

--- server/routes/emails.js
+++ server/routes/emails.js
@@ -1,43 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-import nodemailer from "nodemailer";
+import { sendEmailUsingNodeMailer } from "../controllers/emails.js";
 
 dotenv.config();
 const emailRouter = express.Router();
 
-const transporter = nodemailer.createTransport({
-  service: process.env.NODEMAILER_SERVICE_PROVIDER,
-  host: process.env.NODEMAILER_HOST,
-  port: process.env.NODEMAILER_PORT,
-  secure: true,
-  auth: {
-    user: process.env.NODEMAILER_USER,
-    pass: process.env.NODEMAILER_PASS,
-  },
-});
-
-emailRouter.post("/send", async (req, res) => {
-  const { emailHtml, userEmail } = req.body;
-
-  try {
-    const options = {
-      from: "Musify <musify@gmail.com>",
-      to: userEmail,
-      subject: "Musify Newsletter",
-      html: emailHtml,
-    };
-
-    const email = await transporter.sendMail(options);
-
-    if (!email) {
-      return res.status(404).json({ error: "Email not sent" });
-    }
-
-    return res.status(201).json({ email });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
-  }
-});
+emailRouter.post("/send", sendEmailUsingNodeMailer);
 
 export default emailRouter;
