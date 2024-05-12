1b2a65008024b029f9edd022aa761cac1a7e1a4c
ravindu0823 <guestpc87@gmail.com>
2024-05-13 01:52:49 +0530

    feat: Update ProfileMenu component to include user profile image
    The ProfileMenu component has been updated to include the user profile image. This change allows the user's profile picture to be displayed in the menu. The image source is now dynamic and can be passed as a prop to the component.

--- client/src/components/ProfileMenu.jsx
+++ client/src/components/ProfileMenu.jsx
@@ -11,7 +11,7 @@ import {
 import propTypes from "prop-types";
 import { createElement, useState } from "react";
 
-const ProfileMenu = ({ profileMenuItems }) => {
+const ProfileMenu = ({ profileMenuItems, image }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
@@ -25,15 +25,21 @@ const ProfileMenu = ({ profileMenuItems }) => {
         >
           <Avatar
             variant="circular"
-            size="sm"
+            size="md"
+            withBorder={true}
+            color="blue"
             placeholder={true}
             alt="tania andrew"
             className="border border-gray-900 p-0.5"
-            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+            src={
+              image
+                ? image
+                : "https://docs.material-tailwind.com/img/face-2.jpg"
+            }
           />
           <ChevronDownIcon
-            strokeWidth={2.5}
-            className={`h-3 w-3 transition-transform ${
+            strokeWidth={2}
+            className={`h-4 w-4 transition-transform text-white ${
               isMenuOpen ? "rotate-180" : ""
             }`}
           />
@@ -77,6 +83,7 @@ const ProfileMenu = ({ profileMenuItems }) => {
 
 ProfileMenu.propTypes = {
   profileMenuItems: propTypes.array.isRequired,
+  image: propTypes.string.isRequired,
 };
 
 export default ProfileMenu;
