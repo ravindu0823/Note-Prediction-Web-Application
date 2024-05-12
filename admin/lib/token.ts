91902a0bdae3c98213b0897132bb63fefc649584
ravindu0823 <guestpc87@gmail.com>
2024-05-13 01:50:50 +0530

    chore: Add deleteCookie function and validateCookieExpire function

--- admin/lib/token.ts
+++ admin/lib/token.ts
@@ -1,4 +1,5 @@
 "use server";
+import axios, { VALIDATE_ADMIN } from "@/axios/axios";
 import { cookies } from "next/headers";
 
 export async function setCookie(name: string, value: string) {
@@ -11,7 +12,7 @@ export async function setCookie(name: string, value: string) {
 }
 
 export async function getCookie(name: string) {
-  return await cookies().get(name);
+  return cookies().get(name);
 }
 
 export async function validateCookie(name: string) {
@@ -20,4 +21,26 @@ export async function validateCookie(name: string) {
   }
 
   return true;
+}
+
+export async function deleteCookie(name: string) {
+  await cookies().delete(name);
+}
+
+export async function validateCookieExpire(name: string) {
+  const token = await cookies().get(name);
+
+  try {
+    const res = await axios.get(VALIDATE_ADMIN, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!res.statusText) return false;
+
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
