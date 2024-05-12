d8e8a0e34e0496f0a1e915b85db40ba3b8971cdf
ravindu0823 <guestpc87@gmail.com>
2024-05-13 01:50:02 +0530

    chore: Update Musify Admin Dashboard background image and logo

--- admin/app/(auth)/(signin)/page.tsx
+++ admin/app/(auth)/(signin)/page.tsx
@@ -1,34 +1,20 @@
 import UserAuthForm from "@/components/forms/user-auth-form";
+import Image from "next/image";
 
 export default function AuthenticationPage() {
   return (
     <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
         <div className="absolute inset-0 bg-[url('https://klang.io/wp-content/uploads/2024/02/klangio_background_tinified.jpg')] bg-no-repeat bg-cover bg-center bg-blend-multiply" />
-        <div className="relative z-20 flex items-center text-lg font-medium">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-6 w-6"
-          >
-            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-          </svg>
-          Logo
-        </div>
-        <div className="relative z-20 mt-auto">
-          <blockquote className="space-y-2">
-            <p className="text-lg">
-              &ldquo;This library has saved me countless hours of work and
-              helped me deliver stunning designs to my clients faster than ever
-              before.&rdquo;
-            </p>
-            <footer className="text-sm">Sofia Davis</footer>
-          </blockquote>
+        <div className="relative z-20 flex items-center text-3xl font-bold gap-4">
+          <Image
+            src="/assets/images/musify_logo.webp"
+            alt="Klang Logo"
+            className="rounded-full"
+            width={100}
+            height={100}
+          />
+          Musify
         </div>
       </div>
       <div className="p-4 lg:p-8 h-full flex items-center">
